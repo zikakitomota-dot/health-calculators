d6a1040b22943a99dd0ba1bacb68e5ed9bf02e16
zikakitomota-dot <zikakitomota@gmail.com>
2026-08-05 22:02:31 +0800

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
 
 export default defineConfig({
-  site: "https://health-calculators.zikakitomota.workers.dev",
+  site: "https://health.zaleastudio.com",
   output: "static",
   integrations: [sitemap()],
   compressHTML: true,
